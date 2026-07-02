c2fa2aba454672c3b66efabda7b30aab6b30ded8
bahiizi11 <a.manzi@alustudent.com>
2026-07-02 02:09:02 -0700

    Fix 8-concat_edges.py to use slicing only, no new literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming with Python"
-message = str1 + " " + str2
-print(message)
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[105:111] + str[0:6]
+print(str)
